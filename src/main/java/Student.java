8a606f51a3d4ade07e4c421124972a88e17fa281
ksenko <ksenko.hm@gmail.com>
2023-10-15 20:32:35 +0300

    add smth in student

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,5 +1,8 @@
 public class Student {
     public static void main(String[] args) {
         System.out.println("student??");
+        int a = 20;
+        a =+ 1;
+        System.out.println(a);
     }
 }
